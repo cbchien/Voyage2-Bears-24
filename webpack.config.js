727e0077f70fbc5062414b8660ab7ae31a7304d9
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-13 03:14:19 +0800

    fix(webpack): exclude socket.io client from being included in bundle
    The socket.io-client is provided by Socket.io server

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,9 @@ module.exports = (env = {}) => {
     resolve: {
       extensions: ['.js']
     },
+    externals: {
+      'socket.io-client': 'io'
+    },
     module: {
       rules: [
         {
