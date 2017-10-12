4b0a04cad033869cc00dbc9d35e777c322a69636
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-13 03:18:45 +0800

    fix(serverConfig): move configuration to separate file

---
+++ src/server/config.js
@@ -0,0 +1,19 @@
+const session = require('express-session')
+const path = require('path')
+
+const config = {
+  assets: {
+    javascript: path.resolve(__dirname, '../../build'),
+    style: path.resolve(__dirname, '../../node_modules/antd/dist'),
+    asset: path.resolve(__dirname, './assets'),
+  },
+  middleware: {
+    session: session({
+      secret: process.env.SECRET || 'chingu dashboard',
+      resave: false,
+      saveUninitialized: true,
+    }),
+  },
+}
+
+module.exports = config
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const path = require('path')
-const app = express.Router()
+const config = require('./config')
+const app = express()
 
-const config = {
-  assets: {
-    javascript: path.resolve(__dirname, '../../build'),
-    style: path.resolve(__dirname, '../../node_modules/antd/dist'),
-  },
-}
-
+app.use(config.middleware.session)
 app.use('/build', express.static(config.assets.javascript))
 app.use('/style', express.static(config.assets.style))
+app.use('/asset', express.static(config.assets.asset))
 
 app.get('*', (req, res) => {
   res.sendFile('default.html', { root: path.resolve(__dirname, './view') })
