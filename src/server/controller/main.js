978d47b07ccb4872d7e2381d3cb81e51f1122a5a
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-10-15 21:11:09 +0800

    add function to add sheet ID to 'linkedsheets'

--- src/server/controller/main.js
+++ src/server/controller/main.js
@@ -1,5 +1,17 @@
+const gsheets = require('../model/gsheets')
 const { ServerNamespace } = require('./utils')
 
-class Main extends ServerNamespace {}
+class Main extends ServerNamespace {
+  async addLinkedSheet(sheetID) {
+    const settingsID = gsheets.settingsDocID
+    const listOfSheets = await gsheets.getListOfSheets(settingsID)
+    // linkedsheets is the third sheet (index 2)
+    const index = 2
+    const rows = await gsheets.getRows(settingsID, listOfSheets[index])
+    const append = await gsheets.appendRows(settingsID, listOfSheets[index], [
+      [sheetID]
+    ])
+  }
+}
 
 module.exports = Main
