54037bf52b59dfe19a9523caed53eb9d7dd9fcc4
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-16 13:20:46 +0800

    fix(main.js, server): remove model method from service

--- src/server/controller/main.js
+++ src/server/controller/main.js
@@ -1,17 +1,5 @@
-const gsheets = require('../model/gsheets')
 const { ServerNamespace } = require('./utils')
 
-class Main extends ServerNamespace {
-  async addLinkedSheet(sheetID) {
-    const settingsID = gsheets.settingsDocID
-    const listOfSheets = await gsheets.getListOfSheets(settingsID)
-    // linkedsheets is the third sheet (index 2)
-    const index = 2
-    const rows = await gsheets.getRows(settingsID, listOfSheets[index])
-    const append = await gsheets.appendRows(settingsID, listOfSheets[index], [
-      [sheetID]
-    ])
-  }
-}
+class Main extends ServerNamespace {}
 
 module.exports = Main
