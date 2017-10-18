9823d17250212f46887a17f48f85884a12513565
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-18 12:42:06 +0800

    feat(Login,view): remove header and center login form
    Additionally added a background image

--- src/client/view/Login/index.js
+++ src/client/view/Login/index.js
@@ -1,38 +1,18 @@
 import React from 'react'
-import Layout from 'antd/es/layout'
-import Icon from 'antd/es/icon'
-import Menu from 'antd/es/menu'
-import Avatar from 'antd/es/avatar'
 import LoginForm from './loginForm'
 import {
-  MainLayout,
-  MainHeader,
-  Row,
   Col,
+  Layout,
+  MainLayout,
+  Row,
+  LoginBackground,
 } from './../../component/index'
 const { Content } = Layout
 
-const UserItem = (
-  <Content style={{ display: 'flex', alignItems: 'center', height: '32px' }}>
-    <Avatar
-      size="small"
-      style={{ background: '#a31837', paddingLeft: '5px' }}
-      icon="user"
-    />
-    <span style={{ marginLeft: '5px' }}>
-      eddyw
-    </span>
-  </Content>
-)
-
 const Login = () => (
   <MainLayout>
-    <MainHeader>
-      <Menu.SubMenu title={UserItem}>
-        <Menu.Item key=":logout"><Icon type="logout" /> Logout</Menu.Item>
-      </Menu.SubMenu>
-    </MainHeader>
-    <Row type="flex" justify="center" align="top">
+    <LoginBackground />
+    <Row type="flex" justify="center" align="middle" style={{ height: '100vh' }}>
       <Col xs={24} sm={12} md={10} lg={8} xl={6}>
         <Content style={{ padding: '0 50px' }}>
           <Layout style={{ padding: '24px 0', background: '#fff' }}>
