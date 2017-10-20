42978097bb13e277177cffb8d357af7e4af6972e
Brian <cbchien46@gmail.com>
2017-10-20 13:12:15 +0800

    remove duplicate code and find syntax

--- src/server/model/users.js
+++ src/server/model/users.js
@@ -2,51 +2,52 @@ const debugUser = require('debug')('api:users')
 const gsheets = require('./gsheets')
 
 class DashboardUsers {
-	constructor() {
-		this.validated = null
-	}
+  constructor() {
+    this.validated = null
+  }
 
-	initialize() {
-	}
+  initialize() {
+  }
 
-	async matchLogin(username, password) {
-		// rows parameter input from gsheet getRows function
-		// username and password will be from client side input
-		const settingID = gsheets.settingsDocID
-		const listOfSheets = await gsheets.getListOfSheets(settingID)
-		const rows = await gsheets.getRows(settingID, listOfSheets[1])
-		let matchCount = 0
+  async parseUserRows() {
+    const settingID = gsheets.settingsDocID
+    const listOfSheets = await gsheets.getListOfSheets(settingID)
+    const userRows = await gsheets.getRows(settingID, listOfSheets[1])
+    return userRows
+  }
 
-		for (var row of rows) {
-			if (row[0] === username && row[1] === password) {
-				matchCount++
-			} 
-		}
+  async matchLogin(username, password) {
+    // rows parameter input from gsheet getRows function
+    // username and password will be from client side input
+    const users = await this.parseUserRows()
+    let matchCount = 0
+    for (let row of users) {
+      if (row[0] === username && row[1] === password) {
+        matchCount++
+      } 
+    }
 
-		// return true if only one entry is matched
-		if (matchCount === 1) {
-			this.validated = true
-			debugUser('Matching profile found for:', username)
-			return true
-		} else if (matchCount > 1) {
-			debugUser('duplicate user entry in database')
-			return false
-		} else {
-			return false
-		}
-	}
+    // return true if only one entry is matched
+    if (matchCount === 1) {
+      this.validated = true
+      debugUser('Matching profile found for:', username)
+      return true
+    } else if (matchCount > 1) {
+      debugUser('duplicate user entry in database')
+      return false
+    }
+    return false
+  }
 
-	async getUsers() {
-		// rows parameter input from gsheet getRows function
-		const settingID = gsheets.settingsDocID
-		const listOfSheets = await gsheets.getListOfSheets(settingID)
-		const rows = await gsheets.getRows(settingID, listOfSheets[1])
-		var userArr = []
-		for (var row of rows) {
-			userArr.push(row[0])
-		}
-		return userArr
-	}
+  async getUsers() {
+    // rows parameter input from gsheet getRows function
+    const users = await this.parseUserRows()
+    let userArr = []
+    for (let row of users) {
+      userArr.push(row[0])
+    }
+    return userArr
+  }
 }
 
 module.exports = new DashboardUsers()
