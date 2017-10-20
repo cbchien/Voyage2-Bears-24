2a42fa91ad4c785b7bb16e708663cecbecf0a636
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-20 21:37:06 +0800

    feat(login.js): add login event listener

--- src/server/controller/login.js
+++ src/server/controller/login.js
@@ -1,11 +1,34 @@
 const { ServerNamespace } = require('./utils')
 const users = require('../model/users')
-const debugLogin = require('debug')('test:login')
+const debugLogin = require('debug')('service:Login')
 
 class Login extends ServerNamespace {
-  async connection(myUser = 'test', myPwd = '1234') {
-    const matchUser = await users.matchLogin(myUser, myPwd)
-    debugLogin(matchUser)
+  /**
+   * Attempts to login with the provided credentials
+   * @param {Object} data - user data
+   * @param {string} data.username - username
+   * @param {string} data.password - user password
+   * @param {Function} reply - callback to reply to user
+   */
+  async login(data, reply) {
+    try {
+      const { username, password } = data
+      const { request } = this.socket.request
+      await users.matchLogin(username, password)
+      request.session = {
+        logged: true,
+        username,
+      }
+      reply({
+        message: 'Login successfull',
+      })
+      debugLogin('User logged in successfully')
+    } catch (error) {
+      reply({
+        error: error.message,
+      })
+      debugLogin(error.message)
+    }
   }
 }
 
