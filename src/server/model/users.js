2bf410655d0008664709d1d3389e405760fd7d8c
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-20 20:54:44 +0800

    feat(users.js) add jsdocs and throw error when false
    Simplified matchLogin and added throw on error
    to fullfill acceptance criteria

--- src/server/model/users.js
+++ src/server/model/users.js
@@ -1,40 +1,47 @@
-const debugUser = require('debug')('api:users')
 const gsheets = require('./gsheets')
 
 class DashboardUsers {
+  /**
+   * Gets the list of users from the `users` sheet
+   * @return {Promise<Array>} list of users in the format [[username, password], ...]
+   */
   async parseUserRows() {
     const settingID = gsheets.settingsDocID
     const listOfSheets = await gsheets.getListOfSheets(settingID)
+    const userSheet = listOfSheets.find(sheet => sheet.title === 'users')
+    if (!userSheet) {
+      throw new Error('"users" sheet couldn\'t be found :(')
+    }
     const userRows = await gsheets.getRows(settingID, listOfSheets[1])
     return userRows
   }
-
+  /**
+   * Verifies if the provided username and password exist in the `users` sheet
+   * @param {string} username - username
+   * @param {string} password - password
+   * @return {Promise<boolean>} true if successful. Throws an error otherwise
+   */
   async matchLogin(username, password) {
-    // rows parameter input from gsheet getRows function
-    // username and password will be from client side input
     const users = await this.parseUserRows()
-    let matchCount = 0
-    users.forEach((row) => {
-      if (row[0] === username && row[1] === password) {
-        /* eslint no-plusplus: "error" */
-        matchCount += 1
+    const matchCount = users.reduce((foundCount, [rowUsr, rowPwd]) => (
+      rowUsr === username &&
+      rowPwd === password
+        ? foundCount + 1
+        : foundCount
+    ), 0)
+    if (matchCount) {
+      if (matchCount > 1) {
+        throw new Error('Duplicated user entry in database')
       }
-    })
-
-    // return true if only one entry is matched
-    if (matchCount === 1) {
-      this.validated = true
-      debugUser('Matching profile found for:', username)
       return true
-    } else if (matchCount > 1) {
-      debugUser('duplicate user entry in database for:', username)
-      return false
     }
-    return false
+    throw new Error(`No matching profile was found for ${username}`)
   }
-
+  /**
+   * Gets the list of usernames from `users` sheet
+   * @return {Promise<Array>} list of users in the format [username, ...]
+   */
   async getUsers() {
-    // rows parameter input from gsheet getRows function
     const users = await this.parseUserRows()
     return users.map(row => row[0])
   }
