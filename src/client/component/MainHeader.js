0244f974c193a92c3559d79d6fef36e2e4b02d85
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-22 02:40:40 +0800

    feat(ChinguLogoHeader): allow change default title and text color

--- src/client/component/MainHeader.js
+++ src/client/component/MainHeader.js
@@ -12,14 +12,24 @@ const { Header } = Layout
 
 class MainHeader extends React.PureComponent {
   static propTypes = {
-    children: propTypes.node.isRequired,
+    children: propTypes.node,
+    title: propTypes.string,
+    titleColor: propTypes.string,
+  }
+  static defaultProps = {
+    children: null,
+    title: 'Dashboard',
+    titleColor: '#000',
   }
   render() {
     return (
       <Header style={{ background: 'none', zIndex: 1 }}>
         <Row type="flex" justify="space-between">
           <Col span={10}>
-            <ChinguLogoHeader />
+            <ChinguLogoHeader
+              title={this.props.title}
+              color={this.props.titleColor}
+            />
           </Col>
           <Col span={14} style={{ display: 'flex', justifyContent: 'flex-end' }}>
             <Menu
