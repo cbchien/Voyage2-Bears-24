c393c6595d50dbc5f873f06d3405d10a4190a797
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-22 12:03:12 +0800

    feat(setup.js,service): implement setup service

---
+++ src/client/service/setup.js
@@ -0,0 +1,70 @@
+import {
+  Service,
+  Namespace,
+} from './utils'
+
+@Namespace('/setup')
+class Setup extends Service {
+  state = {
+    step: 0,
+    authUrl: null,
+  }
+  type = {
+    SET_STEP: Symbol('SET_STEP'),
+    SET_AUTH_URL: Symbol('SET_AUTH_URL'),
+  }
+  setCredentials(form) {
+    const {
+      data,
+      replyForm,
+      changeState,
+    } = form
+    changeState('pending')
+    this.askServer('setCredentials', data, (answer) => {
+      changeState('resolved')
+      if (answer.hasError) {
+        replyForm(answer.generalError, answer.fieldErrors)
+      } else if (answer.status === 'OK!') {
+        this.dispatchAs(this.type.SET_STEP, {
+          step: this.state.step + 1,
+        })
+      }
+    })
+  }
+  getAuthUrl(form) {
+    const { replyForm, changeState } = form
+    changeState('pending')
+    replyForm(null, {
+      url: { validateStatus: 'validating' },
+    })
+    this.askServer('getAuthUrl', null, (answer) => {
+      replyForm(null, {
+        url: { validateStatus: 'success' },
+      })
+      this.dispatchAs(this.type.SET_AUTH_URL, {
+        authUrl: answer.url,
+      })
+      changeState('resolved')
+    })
+  }
+  authorize(form) {
+    const {
+      data,
+      replyForm,
+      changeState,
+    } = form
+    changeState('pending')
+    this.askServer('authorize', data, (answer) => {
+      changeState('resolved')
+      if (answer.hasError) {
+        replyForm(answer.generalError, answer.fieldErrors)
+      } else {
+        this.dispatchAs(this.type.SET_STEP, {
+          step: this.state.step + 1,
+        })
+      }
+    })
+  }
+}
+
+export default new Setup()
