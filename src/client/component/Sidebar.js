4758639eda29dad765b10821194bfae58a5e5716
Brian <cbchien46@gmail.com>
2017-10-23 16:05:27 +0800

    props.sections as an array of object

--- src/client/component/Sidebar.js
+++ src/client/component/Sidebar.js
@@ -6,65 +6,52 @@ import {
   Button,
 } from './index'
 
-const { SubMenu } = Menu
-
 class Sidebar extends React.PureComponent {
   static propTypes = {
     sections: propTypes.array.isRequired,
+    defaultSelectedKeys: propTypes.array.isRequired,
   }
   static defaultProps = {
     sections: [
       { item: 'Title1', path: '/page1' },
       { item: 'Title2', path: '/page2' },
       { item: 'Title3', path: '/page3' },
+      { item: 'Title4', path: '/page4' },
     ],
+    defaultSelectedKeys: ['1'],
   }
-  // Okay to use state here? 
+
   state = {
     collapsed: false,
   }
+
   toggleCollapsed = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     })
   }
 
-  SidebarItem = () => (
-    // Need to use .map or sort to return elements form this.props.sections
-    <Menu
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      theme="dark"
-      inlineCollapsed={this.state.collapsed}
-    >
-      <Menu.Item key="1">
-        <Icon type="pie-chart" />
-        <span>Option 1</span>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Icon type="desktop" />
-        <span>Option 2</span>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Icon type="inbox" />
-        <span>Option 3</span>
-      </Menu.Item>
-      <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-        <Menu.Item key="5">Option 5</Menu.Item>
-        <Menu.Item key="6">Option 6</Menu.Item>
-        <Menu.Item key="7">Option 7</Menu.Item>
-        <Menu.Item key="8">Option 8</Menu.Item>
-      </SubMenu>
-    </Menu>
-  )
 
   render() {
+    const list = this.props.sections.map(element =>
+      (<Menu.Item key={element.path}>
+        <Icon type="desktop" />
+        <span><a href={element.path}>{element.item}</a></span>
+      </Menu.Item>),
+    )
     return (
-      <div style={{ width: 240 }}>
-        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+      <div style={{ width: 240, float: 'left', position: 'absolute' }}>
+        <Button type="primary" onClick={this.toggleCollapsed} style={{ margin: '10px 0px 2px 10px' }}>
           <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
         </Button>
+        <Menu
+          defaultSelectedKeys={this.props.defaultSelectedKeys}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={this.state.collapsed}
+        >
+          { list }
+        </Menu>
       </div>
     )
   }
