45434413b331a061bcb7724e262edd839dc42cbd
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-26 12:04:17 +0800

    fix(index.js,service): remove reference to MainProvider in services

--- src/client/service/index.js
+++ src/client/service/index.js
@@ -1,11 +1,9 @@
 import main from './main'
 import setup from './setup'
 import login from './login'
-import MainServiceProvider from './MainServiceProvider'
 
 export default {
   main,
   setup,
   login,
-  MainServiceProvider,
 }
