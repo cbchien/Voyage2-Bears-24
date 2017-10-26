9c3a946cdf9d0b122659c116e2b4ebf978fe59ec
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-26 12:12:01 +0800

    fix(index.js,component): remove reference to sidebar

--- src/client/component/index.js
+++ src/client/component/index.js
@@ -4,7 +4,6 @@ export { default as LoginBackground } from './LoginBackground'
 export { default as MainHeader } from './MainHeader'
 export { default as MainLayout } from './MainLayout'
 export { default as ServiceForm } from './ServiceForm'
-export { default as Sidebar } from './Sidebar'
 export { default as SwitchStep } from './SwitchStep'
 // decorators
 export { bind, registerPath } from './decorators'
