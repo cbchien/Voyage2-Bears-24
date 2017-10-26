9a3723922fa2629c7203175377dad172c2dfe4f1
Eddy Wilson <eddyw.dev@outlook.com>
2017-10-26 12:07:16 +0800

    fix(UserMenu): remove useless component

--- src/client/component/index.js
+++ src/client/component/index.js
@@ -6,6 +6,5 @@ export { default as MainLayout } from './MainLayout'
 export { default as ServiceForm } from './ServiceForm'
 export { default as Sidebar } from './Sidebar'
 export { default as SwitchStep } from './SwitchStep'
-export { default as UserMenu } from './UserMenu'
 // decorators
 export { bind, registerPath } from './decorators'
