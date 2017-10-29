2777467465b3a9ccc104bc773fc5e6b705be467a
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-10-29 14:48:17 +0800

    Add "add link" button, include icons, and set table keys to "spreadsheetId"

--- src/client/view/LinkedSheets/index.js
+++ src/client/view/LinkedSheets/index.js
@@ -4,6 +4,9 @@ import {
   Table,
   Popconfirm,
   message,
+  Row,
+  Col,
+  Icon,
 } from 'antd'
 import CommonView from '../CommonView'
 import {
@@ -45,57 +48,56 @@ class LinkedSheets extends React.PureComponent {
       key: 'action',
       render: id => (
         <Popconfirm title="Are you sure you want to unlink this sheet?" onConfirm={this.unlinkSheet(id)} okText="Yes" cancelText="No">
-          <Button type="danger">Unlink</Button>
+          <Button type="danger"><Icon type="close" />Unlink</Button>
         </Popconfirm>
       ),
     }]
 
     const linkedSheets = [{
-      key: '1',
       name: 'Voyage 1',
       spreadsheetId: '1k5b22g5j',
     }, {
-      key: '2',
       name: 'Voyage 2',
       spreadsheetId: '2k5b22g5j',
     }, {
-      key: '3',
       name: 'Voyage 3',
       spreadsheetId: '3k5b22g5j',
     }, {
-      key: '4',
       name: 'Voyage 4',
       spreadsheetId: '4k5b22g5j',
     }, {
-      key: '5',
       name: 'Voyage 5',
       spreadsheetId: '5k5b22g5j',
     }, {
-      key: '6',
       name: 'Voyage 6',
       spreadsheetId: '6k5b22g5j',
     }, {
-      key: '7',
       name: 'Voyage 7',
       spreadsheetId: '7k5b22g5j',
     }, {
-      key: '8',
       name: 'Voyage 8',
       spreadsheetId: '8k5b22g5j',
     }, {
-      key: '9',
       name: 'Voyage 9',
       spreadsheetId: '9k5b22g5j',
     }, {
-      key: '10',
       name: 'Voyage 10',
       spreadsheetId: '105b22g5j',
+    }, {
+      name: 'Voyage 11',
+      spreadsheetId: '115b22g5j',
     }]
 
     return (
       <CommonView>
         {/* <Table columns={columns} dataSource={linkedSheets} pagination={{ pageSize: 5 }} /> */}
-        <Table columns={columns} dataSource={linkedSheets} />
+        <Row type="flex" justify="end">
+          <Col>
+            <Button type="primary"><Icon type="link" />Add Link</Button>
+          </Col>
+        </Row>
+        <br />
+        <Table rowKey="spreadsheetId" columns={columns} dataSource={linkedSheets} />
       </CommonView>
     )
   }
