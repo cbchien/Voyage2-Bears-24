bf793a96fc47236911ce7b23969ba5618178535d
Eddy Wilson <eddyw.dev@outlook.com>
2017-11-01 12:23:59 +0800

    fix(LinkedSheets,index.js): remove logic, only manage UI state

--- src/client/view/LinkedSheets/index.js
+++ src/client/view/LinkedSheets/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {
   Button,
-  Table,
-  Popconfirm,
-  message,
-  Row,
   Col,
   Icon,
+  message,
+  Popconfirm,
+  Row,
+  Table,
 } from 'antd'
-import GSheetsHelper from '../../helper/gsheets'
 import CommonView from '../CommonView'
-import AddLinkedSheetModal from './AddLinkedSheetModal'
+import LinkedSheetModal from './LinkedSheetModal'
 import {
   registerPath,
+  bind,
 } from '../../component'
 
 @registerPath({
@@ -20,19 +20,19 @@ import {
   title: 'Linked Sheets',
   icon: 'file-excel',
 })
-
 class LinkedSheets extends React.PureComponent {
-  getGoogleSheetURL(id) {
-    return `https://docs.google.com/spreadsheets/d/${id}`
+  state = {
+    isModalVisible: false,
   }
-
-  unlinkSheet(id) {
-    return () => {
-      // TODO: actual logic for unlinking sheet
-      message.success(`Unlinked sheet with ID: ${id}`)
-    }
+  @bind toggleModal() {
+    this.setState({
+      isModalVisible: !this.state.isModalVisible,
+    })
   }
-
+  @bind handleOnOk() {
+    this.toggleModal()
+    message.success('Google Sheet Document linked successfully!')
+  }
   render() {
     const columns = [{
       title: 'Name',
@@ -42,15 +42,22 @@ class LinkedSheets extends React.PureComponent {
       title: 'Spreadsheet ID',
       dataIndex: 'spreadsheetId',
       key: 'spreadsheetId',
-      render: id => <a href={GSheetsHelper.getSheetUrlFromId(id)}>{id}</a>,
+      render: id => (
+        <a href={`https://docs.google.com/spreadsheets/d/${id}`}>{id}</a>
+      ),
     }, {
       title: 'Action',
       dataIndex: 'spreadsheetId',
       key: 'action',
       fixed: 'right',
       width: 100,
-      render: id => (
-        <Popconfirm title="Are you sure you want to unlink this sheet?" onConfirm={this.unlinkSheet(id)} okText="Yes" cancelText="No">
+      render: () => (
+        <Popconfirm
+          title="Are you sure you want to unlink this sheet?"
+          onConfirm={() => {}}
+          okText="Yes"
+          cancelText="No"
+        >
           <Button type="danger"><Icon type="close" />Unlink</Button>
         </Popconfirm>
       ),
@@ -99,11 +106,25 @@ class LinkedSheets extends React.PureComponent {
             <h1>Linked Sheets</h1>
           </Col>
           <Col>
-            <AddLinkedSheetModal />
+            <Button
+              type="primary"
+              onClick={this.toggleModal}
+            >
+              <Icon type="link" />Add Link
+            </Button>
+            <LinkedSheetModal
+              onCancel={this.toggleModal}
+              onOk={this.toggleModal}
+              visible={this.state.isModalVisible}
+            />
           </Col>
         </Row>
-        <br />
-        <Table rowKey="spreadsheetId" columns={columns} dataSource={linkedSheets} pagination={{ total: linkedSheets.length, pageSize: 5 }} />
+        <Table
+          rowKey="spreadsheetId"
+          columns={columns}
+          dataSource={linkedSheets}
+          pagination={{ total: linkedSheets.length, pageSize: 5 }}
+        />
       </CommonView>
     )
   }
