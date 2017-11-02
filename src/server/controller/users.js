68769c3a218ee13e78c099635fafbdbe3e34db62
Brian <cbchien46@gmail.com>
2017-11-02 21:50:04 +0800

    basic error handling

--- src/server/controller/users.js
+++ src/server/controller/users.js
@@ -1,25 +1,50 @@
 const { ServerNamespace } = require('./utils')
 const users = require('../model/users')
+const debugUsers = require('debug')('service:Users')
 
 class Users extends ServerNamespace {
   async connection() {
     await this.fetchUsers()
   }
 
-  async fetchUsers() {
-    const data = await users.getUsers()
-    this.emitClientEvent('fetch users', data)
+  async fetchUsers(reply) {
+    try {
+      const data = await users.getUsers()
+      this.emitClientEvent('fetch users', data)
+      reply({
+        fetchUsers: 'OK'
+      })
+      debugUsers('Successfully fetched authorized users')
+    } catch (error) {
+      reply({
+        hasError: true,
+        generalError: { message: error.message, type: 'error'},
+      })
+      debugUsers(error.message)
+    }
   }
 
-  // functions only. No logic built at this moment
   async deleteUser(username) {
-    const check = await users.deleteUser(username)
-    this.emitClientEvent('delete users', username)
+    try {
+      const check = await users.deleteUser(username)
+      this.emitClientEvent('delete users', username)  
+    } catch (error) {
+      debugUsers(error)
+    }
   }
 
   async updatePassword(username, password) {
-    const check = await users.updateUserPassword(username, password)
-    this.emitClientEvent('Updated password for users', username)
+    const validate = this.hasRequiredFields(username,['username'],true)
+    if (validate.hasError) {
+      return debugUsers(validate)
+    }
+    try {
+      const check = await users.updateUserPassword(username, password)
+      this.emitClientEvent('Updated password for users', username)
+      debugUsers('Updated password for users')
+    } catch (error) {
+      debugUsers(error)
+    }
   }
 }
 
