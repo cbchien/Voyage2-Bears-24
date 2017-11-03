6d7d7b11e10c2d3655ecc97bb66775ee8e6b3dd6
Eddy Wilson <eddyw.dev@outlook.com>
2017-11-03 17:55:49 +0800

    fix(users.js,model): eslint errors

--- src/server/model/users.js
+++ src/server/model/users.js
@@ -50,7 +50,7 @@ class DashboardUsers {
   async deleteUser(username) {
     const allUser = await this.getUsers()
     // add 1 to compensate .shift()
-    const indexToDelete = allUser.findIndex( user => user === username) + 1
+    const indexToDelete = allUser.findIndex(user => user === username) + 1
     // next three const are duplicate codes to parseUserRows()
     const settingID = gsheets.settingsDocID
     const listOfSheets = await gsheets.getListOfSheets(settingID)
@@ -59,13 +59,18 @@ class DashboardUsers {
       throw new Error('"users" sheet couldn\'t be found :(')
     }
     // indexToDelete + 1 to delete single row
-    const removeUser = await gsheets.deleteRows(settingID, userSheet, indexToDelete + 1, indexToDelete + 1)
+    await gsheets.deleteRows(
+      settingID,
+      userSheet,
+      indexToDelete + 1,
+      indexToDelete + 1,
+    )
   }
 
   async updateUserPassword(username, password) {
     const allUser = await this.getUsers()
     // add 2 to compensate .shift()
-    const indexToUpdate = allUser.findIndex( user => user === username) + 2
+    const indexToUpdate = allUser.findIndex(user => user === username) + 2
     // next three const are duplicate codes to parseUserRows()
     const settingID = gsheets.settingsDocID
     const listOfSheets = await gsheets.getListOfSheets(settingID)
@@ -73,9 +78,9 @@ class DashboardUsers {
     if (!userSheet) {
       throw new Error('"users" sheet couldn\'t be found :(')
     }
-    const range = 'A'+indexToUpdate+':B'+indexToUpdate
-    const updatePwd = await gsheets.updateRows(settingID, userSheet, range, [
-      [null, password]
+    const range = `A${indexToUpdate}:B${indexToUpdate}`
+    await gsheets.updateRows(settingID, userSheet, range, [
+      [null, password],
     ])
   }
 }
